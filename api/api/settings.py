8b237edd31419a5c0d3df86ad2f4cbdc3e0a515c
rahulk789 <rahul.u.india@gmail.com>
2022-10-16 13:01:28 +0530

    arwg

--- api/api/settings.py
+++ api/api/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresql-clear-85899',
     }
 }
 
