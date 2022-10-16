ae1e4f8955d5f7a9d3ca93228e957a4af98c89b0
rahulk789 <rahul.u.india@gmail.com>
2022-10-16 12:46:52 +0530

    fix 12356

--- api/api/settings.py
+++ api/api/settings.py
@@ -80,8 +80,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mystats',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
